9a7054a72329f2024fb2a2e04d467d0081584971
JohnM-Kahura <Johnmungai392@gmail.com>
2021-10-31 14:50:41 +0300

    added layouted for settled tab

--- lib/screens/settled.dart
+++ lib/screens/settled.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shark/config/colors.dart';
+import 'package:shark/config/style.dart';
 
 class SettledLoans extends StatefulWidget {
   const SettledLoans({Key? key}) : super(key: key);
@@ -10,6 +12,62 @@ class SettledLoans extends StatefulWidget {
 class _SettledLoansState extends State<SettledLoans> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.builder(
+        itemCount: 13,
+        itemBuilder: (BuildContext context, int index) {
+          return Card(
+            elevation: 1,
+            margin: const EdgeInsets.all(2),
+            child: Container(
+              padding: const EdgeInsets.all(5),
+              color: themeColor,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'John Mungai Kahura',
+                    style: nameStyle,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Phone: ',
+                        style: cardStyle,
+                      ),
+                      Text(
+                        '0746443929',
+                        style: cardStyle,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Loan Amount: ',
+                        style: cardStyle,
+                      ),
+                      Text(
+                        '5000',
+                        style: cardStyle,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'Interest:  ',
+                        style: cardStyle,
+                      ),
+                      Text(
+                        '1000',
+                        style: cardStyle,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
