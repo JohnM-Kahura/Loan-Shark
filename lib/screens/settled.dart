6a44ae48809a7065d869e84737a5d7bc2abe4e35
JohnM-Kahura <Johnmungai392@gmail.com>
2021-11-02 02:28:46 +0300

    get func

--- lib/screens/settled.dart
+++ lib/screens/settled.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:shark/config/colors.dart';
 import 'package:shark/config/style.dart';
@@ -10,64 +11,100 @@ class SettledLoans extends StatefulWidget {
 }
 
 class _SettledLoansState extends State<SettledLoans> {
+  List settledLoans = [];
+
+  final Stream<QuerySnapshot> _activeLoansStream =
+      FirebaseFirestore.instance.collection('Loans').snapshots();
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-        itemCount: 13,
-        itemBuilder: (BuildContext context, int index) {
-          return Card(
-            elevation: 1,
-            margin: const EdgeInsets.all(2),
-            child: Container(
-              padding: const EdgeInsets.all(5),
-              color: themeColor,
+    return StreamBuilder(
+        stream: _activeLoansStream,
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              // provide a refresh button
               child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'John Mungai Kahura',
-                    style: nameStyle,
+                  const Text(
+                    'Something went Wrong please try again later',
+                    style: TextStyle(color: Colors.red),
                   ),
-                  Row(
-                    children: [
-                      Text(
-                        'Phone: ',
-                        style: cardStyle,
-                      ),
-                      Text(
-                        '0746443929',
-                        style: cardStyle,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Loan Amount: ',
-                        style: cardStyle,
-                      ),
-                      Text(
-                        '5000',
-                        style: cardStyle,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Text(
-                        'Interest:  ',
-                        style: cardStyle,
-                      ),
-                      Text(
-                        '1000',
-                        style: cardStyle,
-                      ),
-                    ],
-                  ),
+                  TextButton(
+                      onPressed: () {
+                        // rebuild whole widget
+                      },
+                      child: const Text('Refresh')),
                 ],
               ),
-            ),
-          );
+            );
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView.builder(
+              itemCount: 13,
+              itemBuilder: (BuildContext context, int index) {
+                String name = snapshot.data!.docs[index]['Name'];
+                String amount = snapshot.data!.docs[index]['Amount'];
+                String phone = snapshot.data!.docs[index]['Phone'];
+                String interest = snapshot.data!.docs[index]['Interest'];
+                String status = snapshot.data!.docs[index]['Status'];
+                return Card(
+                  elevation: 1,
+                  margin: const EdgeInsets.all(2),
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    color: themeColor,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          name,
+                          style: nameStyle,
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'Phone: ',
+                              style: cardStyle,
+                            ),
+                            Text(
+                              phone,
+                              style: cardStyle,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'Loan Amount: ',
+                              style: cardStyle,
+                            ),
+                            Text(
+                              amount,
+                              style: cardStyle,
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              'Interest:  ',
+                              style: cardStyle,
+                            ),
+                            Text(
+                              interest,
+                              style: cardStyle,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              });
         });
   }
 }
